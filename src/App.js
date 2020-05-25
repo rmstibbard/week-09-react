64b74093c97d2329c2d8099aa9534ea6ad053847
Richard Stibbard <richardstibbard@gmail.com>
2020-05-25 13:00:14 +0100

    Stuff

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header';
-import Paragraph from './components/Paragraph';
-import Square from './components/Square';
-import People from './components/People';
-
+import Stuff from './components/Stuff';
 
 function App() {
-
   return (
-    <>
-      <Header />
-      <Paragraph />
-      <Square />
-      <People />
-
-    </>
+    <Stuff />
   );
 }
 
---
+++ src/components/Stuff.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header';
+import Paragraph from './Paragraph';
+import Square from './Square';
+import People from './People';
+
+const Stuff = () => (
+    <React.Fragment>
+        <Header />
+        <Paragraph />
+        <Square />
+        <People />
+    </React.Fragment>
+);
+
+export default Stuff;
