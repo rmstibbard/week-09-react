64159b5d96c56016012afbe5331a058522a0497d
Richard Stibbard <richardstibbard@gmail.com>
2020-05-25 12:43:54 +0100

    Header-Paragraph-Square

--- src/App.js
+++ src/App.js
@@ -2,29 +2,17 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
+import Paragraph from './components/Paragraph';
+import Square from './components/Square';
 
 function App() {
 
   return (
     <>
       <Header />
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+      <Paragraph />
+      <Square />
 
-        </header>
-      </div>
     </>
   );
 }
---
+++ src/components/Square.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const squareStyle = {
+    backgroundColor: 'red',
+    height: '200px',
+    width: '200px'
+};
+
+const Square = () => (
+    <div style={squareStyle}></div>
+);
+
+export default Square;
