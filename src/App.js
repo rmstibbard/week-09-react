cd4c0f0b2ebcfcd5fbb3b72037867c1ea13a234b
Richard Stibbard <richardstibbard@gmail.com>
2020-05-25 15:31:19 +0100

    People with props and ternary

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Header text="This amazing text" />
       <Paragraph message="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quamquam tu hanc copiosiorem etiam soles dicere. Ubi ut eam caperet aut quando? Videmus igitur ut conquiescere ne infantes quidem possint. Magna laus. Bonum patria: miserum exilium. Sed tu istuc dixti bene Latine, parum plane. Duo Reges: constructio interrete. Ergo hoc quidem apparet, nos ad agendum esse natos." />
       <Square colour="yellow" />
-      <People />
+      <People names={['James P. Sullivan', 'Mike Wazowski', 'Boo', 'Randall Boggs', 'Roz', 'Fungus']} />
 
     </>
   );
