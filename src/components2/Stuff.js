ace406f85ac6b30305b122b476c59fe815c2b753
Richard Stibbard <richardstibbard@gmail.com>
2020-05-27 09:25:08 +0100

    Rollcall - WORKING

--- src/components2/Stuff.js
+++ src/components2/Stuff.js
@@ -14,7 +14,7 @@ import RollCall from './RollCall';
 
 const Stuff = () => (
     <React.Fragment>
-        <Header>React from Scratch</Header>
+        <Header>Learning React from Scratch</Header>
 
         <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quamquam tu hanc copiosiorem etiam soles dicere. Ubi ut eam caperet aut quando? Videmus igitur ut conquiescere ne infantes quidem possint. Magna laus. Bonum patria: miserum exilium. Sed tu istuc dixti bene Latine, parum plane. Duo Reges: constructio interrete. Ergo hoc quidem apparet, nos ad agendum esse natos.</Paragraph>
 
