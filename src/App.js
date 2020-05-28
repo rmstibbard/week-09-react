0643cf494cfc1afdb76399bce0d0a220fa42878c
Richard Stibbard <richardstibbard@gmail.com>
2020-05-28 10:37:58 +0100

    Routes: 404

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, } from "react-router-dom";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
+
 import Fragment from "./components4/Fragment";
 import Header from "./components4/Header";
 import Content from "./components4/Content";
 import Figure from "./components4/Figure";
+import FourOhFour from "./components4/FourOhFour";
 
 const App = () => (
   <Router>
     <Fragment>
       <Header>App</Header>
-      <Route exact path="/" component={Content}>This is the content</Route>
+      <Switch>
+        <Route exact path="/" component={Content}>This is the content</Route>
 
-      <Route path="/figure">
-        <Figure
-          caption="A cat, strutting its stuff!"
-          src="https://goo.gl/tRdW93"
-        />
-      </Route>
+        <Route path="/figure">
+          <Figure
+            caption="A cat, strutting its stuff!"
+            src="https://goo.gl/tRdW93"
+          />
+        </Route>
+
+        <FourOhFour />
+
+      </Switch>
 
     </Fragment>
   </Router>
---
+++ src/components4/FourOhFour.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class FourOhFour extends Component {
+
+    render() {
+        return (
+            <h2>404 Page not found</h2>
+        )
+    }
+
+}
+
+export default FourOhFour;
